6ede4aafb297b4c0ddfba25b57cbf63b31a4d22b
marcocollado <marco.collado@server.fake>
2014-02-10 19:40:03 -0800

    clone from azure base

---
+++ Includes/simplecms-config.php
@@ -0,0 +1,9 @@
+<?php
+    define('DB_NAME', 'barkerDB');
+    define('DB_USER', 'b0d2b5524d6fb7');
+    define('DB_PASSWORD', '15896bbf');
+    define('DB_HOST', 'ap-cdbr-azure-east-b.cloudapp.net');
+
+    define('DEFAULT_ADMIN_USERNAME', 'WWNPadmin');
+    define('DEFAULT_ADMIN_PASSWORD', 'WWNPadmin');
+?>
