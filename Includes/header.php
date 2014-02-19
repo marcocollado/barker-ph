b54f5f528a93d25ecc0d6ce3d5712649490d53af
lesterdelrosario12 <lester_delrosario12@yahoo.com>
2014-02-19 18:48:33 +0800

    a
    a

--- Includes/header.php
+++ Includes/header.php
@@ -3,19 +3,17 @@
 <html lang="en">
     <head>
         <meta charset="utf-8" />
-        <title>My Site's Title</title>
+        <title>Baker.ph</title>
         <link href="/Styles/Site.css" rel="stylesheet" type="text/css" />
+        <link rel="stylesheet" type="text/css" href="engine1/style.css" />
+	    <script type="text/javascript" src="engine1/jquery.js"></script>
         <meta name="viewport" content="width=device-width, initial-scale=1"> 
-
-
+      
     </head>
     <body>
         <div class="outer-wrapper">
         <header>
-            <div class="content-wrapper">
-                <div class="float-left">
-                    <p class="site-title"><a href="/index.php">My Site</a></p>
-                </div>
+            <div class="content-wrapper">          
                 <div class="float-right">
                     <section id="login">
                         <ul id="login">
@@ -31,10 +29,11 @@
                             }
                         }
                         else
-                        {
-                            echo '<li><a href="/logon.php">Login</a></li>' . "\n";
-                            echo '<li><a href="/register.php">Register</a></li>' . "\n";
-                        }
+                            {
+                                echo '<li><font face="calibri" color="#ba1802" size="4">Are you already a member? Log-in here!&nbsp;&nbsp;</font></li>' . "\n";
+                                echo '<li><a href="/logon.php">Sign in</a></li>' . "\n";
+                                echo '<li><a href="/register.php">Register</a></li>' . "\n";
+                            }
                         ?>
                         </ul>
                         <?php if (logged_on()) {
@@ -47,7 +46,7 @@
             </div>
 
                 <section class="navigation" data-role="navbar">
-                    <nav>
+                    <nav>l
                         <ul id="menu">
                             <li><a href="/index.php">Home</a></li>
                             <?php
--- register.php
+++ register.php
@@ -46,9 +46,13 @@
                     <li>
                         <label for="username">Username:</label> 
                         <input type="text" name="username" value="" id="username" />
-                    </li>
+                    </li> 
                     <li>
                         <label for="password">Password:</label>
+                        <input type="password" name="password" value="" id="password" />
+                    </li>
+                    <li>
+                        <label for="password">Verify Password:</label>
                         <input type="password" name="password" value="" id="password" />
                     </li>
                 </ol>
